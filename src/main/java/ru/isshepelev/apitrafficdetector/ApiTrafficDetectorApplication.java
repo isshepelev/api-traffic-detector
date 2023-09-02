a9ef42961ac6108dc947b5da011ed6819d1b57ed
Ilya <isshepelev2004@gmail.com>
2023-09-02 15:36:06 +0300

    first

---
+++ src/main/java/ru/isshepelev/apitrafficdetector/ApiTrafficDetectorApplication.java
@@ -0,0 +1,13 @@
+package ru.isshepelev.apitrafficdetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTrafficDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTrafficDetectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/isshepelev/apitrafficdetector/ApiTrafficDetectorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isshepelev.apitrafficdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTrafficDetectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
