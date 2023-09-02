8ebc03c6eee842fb4b382c18288d65c4893480e2
Ilya <isshepelev2004@gmail.com>
2023-09-02 21:22:38 +0300

    add class exception

---
+++ src/main/java/ru/isshepelev/apitrafficdetector/exception/DetectorExceptionHandler.java
@@ -0,0 +1,23 @@
+package ru.isshepelev.apitrafficdetector.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class DetectorExceptionHandler {
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> NullPointerException(){
+        return  new ResponseEntity<>("Ошибка в параметрах запроса. Запрос не следует повторять",
+                HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> IllegalArgumentException() {
+        return new ResponseEntity<>(
+                "Ошибка сервера при выполнении запроса. Запрос следует повторить позднее",
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
